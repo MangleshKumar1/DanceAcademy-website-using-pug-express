1127687e47864a0ef3fa35a87f3dac759d728131
Manglesh Kumar <Mgleshkumar42343@gmail.com>
2023-02-19 17:38:23 +0530

    Adding contact.pug functionality

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-This is a Simple Website for Dance Academy using Pug template, express and node.js.
+This is a Simple Website for Dance Academy using Pug template, express and node.js.   an intro to mongoose and mongo
+
+using nodemon to run server
+
+home page is main page 
+contact.pug is a template replaced in contact-info page  wher i used mongoose in app.js to create a schema and model to store the data
+Data created is stored offline for that first need to open mongod process command 
+in another terminal using mongosh command we can get the info of users which they typed
--- app.js
+++ app.js
@@ -3,22 +3,64 @@ const path = require('path');
 const app = express();
 const port = 80;
 
-//express specific stuff
-app.use('/static', express.static('static'));
-// app.use(express.urlencoded);
+// mongoose related stuff for storing data in mongodb using mongoose which use writes
+const mongoose = require('mongoose');
+const bodyparser = require('body-parser');
 
-//pug specific stuff
-app.set('view engine', 'pug');
-app.set('views', path.join(__dirname, 'views'));
 
-// endpoints
-app.get('/', (req, res) => {
-    res.status(200).render('home.pug');
-});
+mongoose.set('strictQuery', true);
+// mongoose.set('strictQuery', false);
+main().catch(err => console.log(err));
 
-app.get('/contact', (req, res) => {
-    res.status(200).render('contact.pug');
-});
-app.listen(port, () => {
-    console.log(`The application started successfully at ${port}`);
-})
+async function main() {
+    await mongoose.connect('mongodb://127.0.0.1:27017/contactDance');
+
+    // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
+   
+    // mongoose schema
+    var contactSchema = new mongoose.Schema({
+        name: String,
+        phone: String,
+        email: String,
+        address: String,
+        description: String
+    });
+
+    // locking mongoose schema and creating mongoose model
+    var Contact = mongoose.model('Contact', contactSchema);
+
+
+    //express specific stuff
+    app.use('/static', express.static('static'));
+    app.use(express.urlencoded());
+
+    //pug specific stuff
+    app.set('view engine', 'pug');
+    app.set('views', path.join(__dirname, 'views'));
+
+    // endpoints
+    app.get('/', (req, res) => {
+        res.status(200).render('home.pug');
+    });
+
+
+    app.get('/contact', (req, res) => {
+        res.status(200).render('contact.pug');
+    });
+
+
+    app.post('/contact', (req, res) => {
+        var myData = new Contact(req.body);
+        myData.save().then(() => {
+            res.send("This item stored in database")
+        }).catch(() => {
+            res.status(400).send("Item not saved");
+        })
+    });
+
+    app.listen(port, () => {
+        console.log(`The application started successfully at ${port}`);
+    })
+
+
+}
